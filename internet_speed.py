8fd92b9f720d67b85f5823265189b7ed08bd4aa0
Muhammad Dawood Moria <muhammaddawoodmoria@gmail.com>
2024-05-25 20:34:22 +0500

    internet_speed.py

--- internet_speed.py
+++ internet_speed.py
@@ -2,7 +2,7 @@ from tkinter import *
 import speedtest
 
 def speedcheck():
-    sp =speedtest.speedtest()
+    sp = speedtest.Speedtest() 
     sp.get_servers()
     down =str(round(sp.download()/(10**6),3)) + "Mbps"
     up =str(round(sp.upload()/(10**6),3)) + "Mbps"
@@ -14,26 +14,26 @@ def speedcheck():
 sp = Tk()
 sp.title("Internet Speed Test")
 sp.geometry("500x650")
-sp.config(bg="Blue")
+sp.config(bg="Green")
 
-lab=Label(sp,text="Internet Speed Test",font=("Time New Roman",40,"bold"),bg="Blue",fg="White")
+lab=Label(sp,text="Internet Speed Test",font=("Time New Roman",30,"bold"),bg="Yellow",fg="Black")
 lab.place(x=60,y=40,height=50,width=380)
 
-lab=Label(sp,text="Downloading speed",font=("Time New Roman",40,"bold"),bg="Blue",fg="White")
+lab=Label(sp,text="Download speed",font=("Time New Roman",30,"bold"),bg="Yellow",fg="Black")
 lab.place(x=60,y=130,height=50,width=380)
 
-lab_down=Label(sp,text="00",font=("Time New Roman",40,"bold"),bg="Blue",fg="White")
+lab_down=Label(sp,text="00",font=("Time New Roman",30,"bold"),bg="Yellow",fg="Black")
 lab_down.place(x=60,y=200,height=50,width=380)
 
-lab=Label(sp,text="Upload Speed",font=("Time New Roman",40,"bold"),bg="Blue",fg="White")
+lab=Label(sp,text="Upload Speed",font=("Time New Roman",30,"bold"),bg="Yellow",fg="Black")
 lab.place(x=60,y=290,height=50,width=380)
 
-lab_up=Label(sp,text="00",font=("Time New Roman",40,"bold"),bg="Blue",fg="White")
+lab_up=Label(sp,text="00",font=("Time New Roman",30,"bold"),bg="Yellow",fg="Black")
 lab_up.place(x=60,y=360,height=50,width=380)
 
 
-button = Button(sp,text="Check Speed",font=("Time New Roman",40,"bold"),relief=RAISED,bg="Green",command=speedcheck)
-button.place()
+button = Button(sp,text="Check Speed",font=("Time New Roman",30,"bold"),relief=RAISED,bg="orange",command=speedcheck)
+button.place(x=60,y=460,height=50,width=380)
 
 
-sp.mainloop(x=60,y=460,height=50,width=380)
+sp.mainloop()
